75bb68fb91cdca4f4e019465adb63467338bcdf8
Pasha Khosravi <khosravipasha@gmail.com>
2019-07-18 15:08:57 -0700

    Vtree parser + tests

--- src/Juice/Juice.jl
+++ src/Juice/Juice.jl
@@ -7,18 +7,21 @@ Circuits:
 
 module Juice
 
-export load_psdd_prob_circuit, FlowCircuit, ProbCircuit,
+export FlowCircuit, ProbCircuit,
     AggregateFlowCircuit, XData, pass_up, log_likelihood_per_instance,
-    load_circuit, ProbCircuit△, fully_factorized_circuit,
+    ProbCircuit△, fully_factorized_circuit,
     log_likelihood, compute_log_likelihood, node_stats, pass_down,
-    FlowCircuitNode, ProbCircuitNode, circuit_matchers, LCDecisionLine,
-    parse_one_obj, LCElementTuple, BiasLine, NegLiteralLine, PosLiteralLine,
-    HeaderLine, CommentLine, parse_lc_file, LogicalCircuitNode, CircuitFormatLine,
+    FlowCircuitNode, ProbCircuitNode, LogicalCircuitNode,
     ProbCircuitNode, CircuitNode, FlowCircuitNode, flatmap, num_parameters,
     estimate_parameters, train_mixture, mnist, FlowCache, reset_aggregate_flows,
     Circuit△, LogicalCircuit△, FlowCircuit△, accumulate_aggr_flows_batch,
     learn_chow_liu_tree, twenty_datasets, dataset, train, WXData, feature_matrix,
-    compile_prob_circuit_from_clt, num_features, clt_likelihood_per_instance, get_infernece
+    compile_prob_circuit_from_clt, num_features, clt_likelihood_per_instance, get_infernece,
+    load_psdd_prob_circuit, load_circuit, circuit_matchers, LCDecisionLine,
+    parse_one_obj, LCElementTuple, BiasLine, NegLiteralLine, PosLiteralLine,
+    HeaderLine, CommentLine, parse_lc_file, CircuitFormatLine,
+    parse_vtree_file, compile_vtree_format_lines,
+    VtreeNode, VtreeLeafNode, VtreeInnerNode, IsLeaf, Variables, VariableCount
 
 using Query
 using IterTools
@@ -32,12 +35,16 @@ using .Data
 using .Utils
 
 include("LogicalCircuits.jl")
-include("CircuitParser.jl")
 include("FlowCircuits.jl")
 include("AggregateFlowCircuits.jl")
 include("ProbCircuits.jl")
+include("Vtree.jl")
+
 include("ProbMixtures.jl")
 include("ChowLiuTree.jl")
+
 include("CircuitBuilder.jl")
+include("IO/CircuitParser.jl")
+include("IO/VtreeParser.jl")
 
 end
