40e46982b5e5a0e57a4c548e5518181ba32dbd45
Guy Van den Broeck <guyvdb@cs.ucla.edu>
2019-08-30 21:06:03 -0700

    Split up long file

--- src/Juice/IO/IO.jl
+++ src/Juice/IO/IO.jl
@@ -9,6 +9,7 @@ export
 load_logical_circuit, load_struct_logical_circuit, load_prob_circuit, load_struct_prob_circuit, load_psdd_logical_circuit,load_lc_logical_circuit, load_psdd_prob_circuit, parse_lc_file, parse_psdd_file,
 
 # CircuitLineCompiler
+# CircuitLineTypes
 # can be kept internal
 
 # CircuitSaver
@@ -20,8 +21,11 @@ parse_vtree_file, compile_vtree_format_lines, load_vtree, save
 include("VtreeLineCompiler.jl")
 include("VtreeParser.jl")
 include("VtreeSaver.jl")
+
+include("CircuitLineTypes.jl")
 include("CircuitLineCompiler.jl")
 include("CircuitParser.jl")
+
 include("CircuitSaver.jl")
 
 end
