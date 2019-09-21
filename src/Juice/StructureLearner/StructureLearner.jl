58632affdaa3910e1bece939b6bf7946c5389d69
mrfolfox4 <arranger1044@aim.com>
2019-09-21 13:53:33 -0700

    Initial work on refactoring learnpsdd and utils

--- src/Juice/StructureLearner/StructureLearner.jl
+++ src/Juice/StructureLearner/StructureLearner.jl
@@ -15,7 +15,7 @@ learn_chow_liu_tree, parent_vector, print_tree, CLT, parse_clt,
 compile_prob_circuit_from_clt, learn_prob_circuit, BaseCache, ⊤, LitCache,
 
 # PSDDInitializer
-learn_vtree_from_clt, compile_psdd_from_clt, set_base,
+learn_vtree_from_clt, compile_psdd_from_clt, set_base, build_clt_structure,
 train_mixture,
 
 # PSDDLearner
@@ -23,7 +23,8 @@ partial_copy, calculate_all_bases, split_operation, parents_vector,
 compile_literal_nodes, compile_true_nodes, compile_decision_nodes, compile_decision_node,
 pick_variable_mi, pick_variable_rand, pick_edge_max_flow, pick_edge_rand,pick_edge_max_gradient,
 pick_edge_and_variable, stop_training, train_bagging, one_bag, save_h5, train_psdd, main_learner, learn_psdd_circuit,
-initialize_mixture_model, main_psdd_learner, print_ll, single_psdd_learner
+initialize_mixture_model, main_psdd_learner, print_ll, single_psdd_learner,
+learn_single_psdd    
 
 include("ChowLiuTree.jl")
 include("CircuitBuilder.jl")
