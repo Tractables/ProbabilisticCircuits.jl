9aac7d8a85fc1916ccce6ed54fe6f74ba28b2471
Guy Van den Broeck <guyvdb@gmail.com>
2019-11-28 14:59:27 -0800

    parallelize test suite

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
-include("../src/Juice/Juice.jl")
-
 # Driver script for all unit tests
+
+# Load Juice code on all processes
+@everywhere include("../src/Juice/Juice.jl")
 
 using Jive
 runtests(@__DIR__)
