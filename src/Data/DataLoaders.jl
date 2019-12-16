50a7eda8e9125371a7796f051f291fc855216e74
Guy Van den Broeck <guyvdb@gmail.com>
2019-12-16 00:30:12 -0800

    removed package DataDeps

--- src/Data/DataLoaders.jl
+++ src/Data/DataLoaders.jl
@@ -1,32 +1,6 @@
-#=
-MyData:
-- Julia version: 1.1.1
-- Author: guy
-- Date: 2019-06-25
-=#
 using MLDatasets
 using CSV
-using DataDeps
 using Pkg.Artifacts
-
-#####################
-# Register data sources with DataDeps
-#####################
-
-function __init__()
-    register(DataDep(
-        "SampledBinaryMNIST",
-        "Sampled Binary MNIST data",
-        expand_folds(x -> "http://www.cs.toronto.edu/~larocheh/public/datasets/binarized_mnist/binarized_mnist_$x.amat"),
-         "56e293409ebbdc08bcc7cbfe5453fbf7a9b86d0bb9b10d38a4919245566b7783"
-    ))
-end
-
-expand_folds(f) = [f("train"), f("valid"), f("test")]
-
-const twenty_dataset_names = ["accidents", "ad", "baudio", "bbc", "bnetflix", "book", "c20ng", "cr52", "cwebkb",
-           "dna", "jester", "kdd", "kosarek", "msnbc", "msweb", "nltcs", "plants", "pumsb_star", "tmovie", "tretail"]
-
 
 #####################
 # Data loaders
@@ -54,24 +28,20 @@ function mnist()
     XYDataset(train,valid,test)
 end
 
-function sampled_mnist()
-    data_dir = datadep"SampledBinaryMNIST"
-    function load(type)
-        dataframe = CSV.read(data_dir*"/binarized_mnist_$type.amat"; header=false, delim=" ",
-                 truestrings=["1"], falsestrings=["0"], type=Bool, strict=true)
-        XData(BitArray(Base.convert(Matrix{Bool}, dataframe)))
-    end
-    train = load("train")
-    valid = load("valid")
-    test = load("test")
-    XDataset(train,valid,test)
-end
+expand_folds(f) = [f("train"), f("valid"), f("test")]
+
+const twenty_dataset_names = ["accidents", "ad", "baudio", "bbc", "bnetflix", "book", "c20ng", "cr52", "cwebkb",
+           "dna", "jester", "kdd", "kosarek", "msnbc", "msweb", "nltcs", "plants", "pumsb_star", "tmovie", "tretail", 
+           "binarized_mnist"]
+
+
+sampled_mnist() = twenty_datasets("binarized_mnist")
 
 function twenty_datasets(name)
     @assert in(name, twenty_dataset_names)
-    data_dir = artifact"twenty_datasets"
+    data_dir = artifact"density_estimation_datasets"
     function load(type)
-        dataframe = CSV.read(data_dir*"/Density-Estimation-Datasets-1.0/datasets/$name/$name.$type.data"; header=false, delim=",",
+        dataframe = CSV.read(data_dir*"/Density-Estimation-Datasets-1.0.1/datasets/$name/$name.$type.data"; header=false,
                  truestrings=["1"], falsestrings=["0"], type=Bool, strict=true)
         XData(BitArray(Base.convert(Matrix{Bool}, dataframe)))
     end
