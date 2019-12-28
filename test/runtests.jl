6120f4fee1426b02297de1cf91ed00d8d7850358
Guy Van den Broeck <guyvdb@gmail.com>
2019-12-27 18:53:58 -0800

    define test environment ad use it in runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,13 @@
 # Driver script for all unit tests
 
+using Distributed
+@everywhere using Pkg
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    # when run as a script, activate the test environment of this package so that test dependencies are available
+    @everywhere Pkg.activate(dirname(@__FILE__));
+end
+
 using Jive
 
 runtests(@__DIR__, skip=["runtests.jl", "helper"])
