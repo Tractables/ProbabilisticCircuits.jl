c6a9700aeb6605992d2fcf1bcfbc873fa8472398
Pasha Khosravi <khosravipasha@gmail.com>
2020-05-11 04:23:40 -0700

    using broadcast! for marginal computation

--- src/Probabilistic/ProbFlowCircuits.jl
+++ src/Probabilistic/ProbFlowCircuits.jl
@@ -4,15 +4,13 @@
 function marginal_pass_up(circuit::UpFlowΔ{O,F}, data::XData{E}) where {E <: eltype(F)} where {O,F}
     resize_flows(circuit, num_examples(data))
     cache = zeros(Float64, num_examples(data)) #TODO: fix type later
-
     marginal_pass_up_node(n::UpFlowΔNode, ::PlainXData) = ()
 
     function marginal_pass_up_node(n::UpFlowLiteral{O,F}, cache::Array{Float64}, data::PlainXData{E}) where {E <: eltype(F)} where {O,F}
         pass_up_node(n, data)
         # now override missing values by 1
         npr = pr(n)
-        missing_features = feature_matrix(data)[:,variable(n)] .< zero(eltype(F))
-        npr[missing_features] .= 1
+        npr[feature_matrix(data)[:,variable(n)] .< zero(eltype(F))] .= 1
         npr .= log.( npr .+ 1e-300 )
         return nothing
     end
@@ -20,21 +18,23 @@ function marginal_pass_up(circuit::UpFlowΔ{O,F}, data::XData{E}) where {E <: el
     function marginal_pass_up_node(n::UpFlow⋀Cached, cache::Array{Float64}, ::PlainXData)
         pr(n) .= 0
         for i=1:length(n.children)
-            pr(n) .+= pr(n.children[i])
+            # pr(n) .+= pr(n.children[i])
+            broadcast!(+, pr(n), pr(n), pr(n.children[i]))
         end
         return nothing
     end
 
     function marginal_pass_up_node(n::UpFlow⋁Cached, cache::Array{Float64}, ::PlainXData)
-        # A simple for loop seems to be way faster than logsumexp because of memory allocations are much lower.
         pr(n) .= 1e-300
-        for i=1:length(n.children)        
-            cache .= (pr(n.children[i]))
-            cache .+= (prob_origin(n).log_thetas[i])
-            cache .= exp.(cache)
-            pr(n) .+= cache
+        for i=1:length(n.children)    
+            cache .= 0
+            # broadcast reduced memory allocation, though accessing prob_origin(n).log_thetas[i] still allocates lots of extra memory, 
+            # it is proabably due to derefrencing the pointer
+            broadcast!(+, cache, pr(n.children[i]), prob_origin(n).log_thetas[i])
+            broadcast!(exp, cache, cache)
+            broadcast!(+, pr(n), pr(n), cache)
         end
-        pr(n) .= log.(pr(n))
+        broadcast!(log, pr(n), pr(n));
         return nothing
     end
 
