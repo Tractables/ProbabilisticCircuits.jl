7f55da5beb8c4554bb40b7224ed72f09e5c4420b
Pasha Khosravi <khosravipasha@users.noreply.github.com>
2020-05-25 14:03:40 -0700

    delete generate_key
    This was not suppoed to be pushed

--- generate_key.jl
+++
@@ -1,6 +0,0 @@
-using ProbabilisticCircuits
-using DocumenterTools
-
-#DocumenterTools.genkeys(ProbabilisticCircuits)
-
-DocumenterTools.genkeys(user="khosravipasha", repo="git@github.com:Juice-jl/ProbabilisticCircuits.jl.git")
