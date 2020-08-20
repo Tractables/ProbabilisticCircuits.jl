68f11feafa6d4bdd4161148a08d0295ab1dec34a
YitaoLiang <yliang@fandm.edu>
2020-08-19 22:10:16 -0500

    [feature] logistic circuit: recover edge flow from v and node_flow

--- src/Logistic/parameter_circuit.jl
+++ src/Logistic/parameter_circuit.jl
@@ -91,18 +91,18 @@ struct CuLayeredParameterCircuit
 end
 
 function class_likelihood(circuit::CuLayeredParameterCircuit, nc::Integer, data::CuMatrix{Float32}, reuse_up=nothing, reuse_down=nothing, reuse_cp=nothing)
-    cw, node_flow, edge_flow, v = class_weights(circuit, nc, data, reuse_up, reuse_down, reuse_cp)
+    cw, flow, v = class_weights(circuit, nc, data, reuse_up, reuse_down, reuse_cp)
     one = Float32(1.0)
-    return @. one / (one + exp(-cw)), node_flow, edge_flow, v
+    return @. one / (one + exp(-cw)), flow, v
 end
 
 function class_weights(circuit::CuLayeredParameterCircuit, nc::Integer, data::CuMatrix{Float32}, reuse_up=nothing, reuse_down=nothing, reuse_cw=nothing)
-    node_flow, edge_flow, v = compute_flows2(circuit.layered_circuit, data, reuse_up, reuse_down)
-    cw = calculate_class_weights(circuit, nc, data, edge_flow, reuse_cw)
-    return cw, node_flow, edge_flow, v
+    flow, v = compute_flows2(circuit.layered_circuit, data, reuse_up, reuse_down)
+    cw = calculate_class_weights(circuit, nc, data, v, flow, reuse_cw)
+    return cw, flow, v
 end
 
-function calculate_class_weights(circuit::CuLayeredParameterCircuit, nc::Integer, data::CuMatrix{Float32}, flow, reuse_cw=nothing)
+function calculate_class_weights(circuit::CuLayeredParameterCircuit, nc::Integer, data::CuMatrix{Float32}, v, flow, reuse_cw=nothing)
     ne = num_examples(data)
     cw = if reuse_cw isa CuMatrix{Float32} && size(reuse_cw) == (ne, nc)
         reuse_cw .= zero(Float32)
@@ -114,18 +114,17 @@ function calculate_class_weights(circuit::CuLayeredParameterCircuit, nc::Integer
     dec_per_thread = 4
     CUDA.@sync for i = 1:length(circuit.layered_circuit.layers)
         circuit_layer = circuit.layered_circuit.layers[i]
-        flow_layer = flow[i]
         parameter_layer = circuit.layered_parameters[i]
         ndl = num_decisions(circuit_layer)
         num_threads = balance_threads(ne, ndl / dec_per_thread, 8)
         num_blocks = ceil(Int, ne / num_threads[1]), ceil(Int, ndl / num_threads[2] / dec_per_thread)
-        @cuda threads=num_threads blocks=num_blocks calculate_class_weights_layer_kernel_cuda(cw, flow_layer, circuit_layer.decisions, parameter_layer)
+        @cuda threads=num_threads blocks=num_blocks calculate_class_weights_layer_kernel_cuda(cw, v, flow, circuit_layer.decisions, circuit_layer.elements, parameter_layer)
     end
     
     return cw
 end
 
-function calculate_class_weights_layer_kernel_cuda(cw, flow, decisions, parameters)
+function calculate_class_weights_layer_kernel_cuda(cw, v, flow, decisions, elements, parameters)
     index_x = (blockIdx().x - 1) * blockDim().x + threadIdx().x
     index_y = (blockIdx().y - 1) * blockDim().y + threadIdx().y
     stride_x = blockDim().x * gridDim().x
@@ -135,12 +134,21 @@ function calculate_class_weights_layer_kernel_cuda(cw, flow, decisions, paramete
     
     for j = index_x:stride_x:ne
         for i = index_y:stride_y:num_decisions
-            first_elem = @inbounds decisions[2, i]
-            last_elem = @inbounds decisions[3, i]
-            for e = first_elem:last_elem
-                # following needs to be memory safe
-                for class=1:nc
-                    @CUDA.atomic cw[j, class] += flow[j, e] * parameters[class, e] # atomic is automatically inbounds
+            decision_id = @inbounds decisions[1, i]
+            n_up = @inbounds v[j, decision_id]
+            if n_up > zero(Float32)
+                first_elem = @inbounds decisions[2, i]
+                last_elem = @inbounds decisions[3, i]
+                n_down = @inbounds flow[j, decision_id]
+                for e = first_elem:last_elem
+                    e1 = @inbounds elements[1,first_elem]
+                    e2 = @inbounds elements[2,first_elem]
+                    e_up = @inbounds (v[j, e1] * v[j, e2])
+                    edge_flow = e_up / n_up * n_down
+                    # following needs to be memory safe
+                    for class=1:nc
+                        @CUDA.atomic cw[j, class] += edge_flow * parameters[class, e] # atomic is automatically inbounds
+                    end
                 end
             end
         end
@@ -148,7 +156,6 @@ function calculate_class_weights_layer_kernel_cuda(cw, flow, decisions, paramete
     
     return nothing
 end
-
 
 function one_hot(labels::Vector, nc::Integer)    
     ne = length(labels) 
