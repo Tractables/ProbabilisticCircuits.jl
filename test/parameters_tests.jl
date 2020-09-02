b2e0fad6b8d68bee99c3e10f81efdafffaeafb57
Guy Van den Broeck <guyvdb@gmail.com>
2020-09-02 02:29:44 -0500

    add tests for likelihood

--- test/parameters_tests.jl
+++ test/parameters_tests.jl
@@ -7,10 +7,23 @@ using DataFrames: DataFrame
     
     dfb = DataFrame(BitMatrix([true false; true true; false true]))
     r = fully_factorized_circuit(ProbCircuit,num_features(dfb))
+    
     estimate_parameters(r,dfb; pseudocount=1.0)
+    @test log_likelihood_avg(r,dfb) ≈ LogicCircuits.Utils.fully_factorized_log_likelihood(dfb; pseudocount=1.0)
 
-    # TODO test identical likelihoods with
-    # ll1  = LogicCircuits.Utils.fully_factorized_log_likelihood(dfb; pseudocount=1)
-    # ll0 = LogicCircuits.Utils.fully_factorized_log_likelihood(dfb)
+    estimate_parameters(r,dfb; pseudocount=0.0)
+    @test log_likelihood_avg(r,dfb) ≈ LogicCircuits.Utils.fully_factorized_log_likelihood(dfb; pseudocount=0.0)
+
+    if CUDA.functional()
+
+        dfb_gpu = to_gpu(dfb)
+        
+        estimate_parameters(r,dfb_gpu; pseudocount=1.0)
+        @test log_likelihood_avg(r,dfb_gpu) ≈ LogicCircuits.Utils.fully_factorized_log_likelihood(dfb; pseudocount=1.0)
+
+        estimate_parameters(r,dfb_gpu; pseudocount=0.0)
+        @test log_likelihood_avg(r,dfb_gpu) ≈ LogicCircuits.Utils.fully_factorized_log_likelihood(dfb; pseudocount=0.0)
+
+    end
 
 end
