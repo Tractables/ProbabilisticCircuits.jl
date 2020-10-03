135d48f79e7721078ddc9d7a02c9e57835b52cdc
Guy Van den Broeck <guyvdb@gmail.com>
2020-10-02 18:36:05 -0700

    travis update

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,5 @@
 # Driver script for all unit tests
 
-using Distributed
-@everywhere using Pkg
-
-if abspath(PROGRAM_FILE) == @__FILE__
-    # when run as a script, activate the test environment of this package so that test dependencies are available
-    @everywhere Pkg.activate(dirname(@__FILE__))
-    @everywhere Pkg.instantiate()
-end
-
 using Jive
 
 # TODO reinstate after refactoring all modules
