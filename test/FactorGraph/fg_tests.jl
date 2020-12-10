9cc24cf792f2c10bf30ef37b2da66836fa837458
Tal Friedman <talf301@gmail.com>
2020-12-09 16:44:06 -0800

    adding factorgraph test

---
+++ test/FactorGraph/fg_tests.jl
@@ -0,0 +1,21 @@
+using Test
+using ProbabilisticCircuits
+using LogicCircuits
+using Pkg.Artifacts
+
+const zoo_version = "/Circuit-Model-Zoo-0.1.2"
+fg_file(name) =
+    artifact"circuit_model_zoo" * zoo_version * "/fgs/$name"
+
+
+@testset "Load small fg and test forward pass" begin
+    file = fg_file("asia.uai")
+    fg = fromUAI(file)
+    @test length(fg.vars) == 8
+    @test legnth(fg.facs) == 8
+    circuit1, var_lits, fac_lits = compile_factor_graph(fg)
+    circuit2 = PlainLogicCircuit(circuit1)
+    circuit3 = smooth(circuit2)
+    gv = get_varprob(fg, var_lits, fac_lits)
+    @test wmc_chavira(circuit3, gv) ≈ 1.0 atol = 1e-6
+end
