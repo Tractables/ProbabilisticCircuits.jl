64c3004e1b8dd21c49170ce3ff3d48bf3b642b69
Guy Van den Broeck <guyvdb@gmail.com>
2021-03-18 15:46:04 -0700

    make BlossomV an optional dependency

--- test/structurelearner/vtree_learner_tests.jl
+++ test/structurelearner/vtree_learner_tests.jl
@@ -14,20 +14,10 @@ using DataFrames
         end
     end
 
-    function test_vtree_variable_partition_broken(v, partition)
-        if v isa PlainVtreeLeafNode
-            @test_skip variable(v) == partition
-        else
-            test_vtree_variable_partition_broken(v.left, partition[1])
-            test_vtree_variable_partition_broken(v.right, partition[2])
-        end
-    end
-
     data = DataFrame(BitArray([1 0 1 0 1 0 1 0 1 0;
                 1 1 1 1 1 1 1 1 1 1;
                 0 0 0 0 0 0 0 0 0 0;
                 0 1 1 0 1 0 0 1 0 1]))
-    p1 = [[[[1,7],[9,10]],[4,6]], [[2,8],[3,5]]]
     p2 = [[[6,4],[2,[10,8]]],[[5,3],[1,[9,7]]]]
     p3 = [1, [[[3,[[2,[8,10]],5]],[4,6]],[7, 9]]]
 
@@ -41,11 +31,5 @@ using DataFrames
         @test num_nodes(v) == 19
     end
 
-    for (alg, vars) in zip([:bottomup], [p1])
-        v = learn_vtree(data; alg=alg)
-        test_vtree_variable_partition_broken(v, vars)
-        @test num_variables(v) == 10
-        @test num_edges(v) == 18
-        @test num_nodes(v) == 19
-    end
+    # NOTE: there are not tests for the bottom-up vtree learner because that requires `using BlossomV` which we don't want as a dependency -- but the functionality is still available for those willing to install Blossom
 end
