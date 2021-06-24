a5ffffd3a16308bb9cbd011fc211edcc6df5dbb6
Renato Lui Geh <renatogeh@gmail.com>
2021-06-24 09:16:22 -0300

    Add Ensemble I/O

--- test/ensembles/bmc_tests.jl
+++ test/ensembles/bmc_tests.jl
@@ -1,13 +1,14 @@
 using Test
 using ProbabilisticCircuits
 using DataFrames
-using BinaryDecisionDiagrams
+using BinaryDecisionDiagrams: Diagram, BinaryDecisionDiagrams
+const BDD = BinaryDecisionDiagrams
 
 @testset "BMC tests with SamplePSDD" begin
     # Set up a logic constraint ϕ as a BDD and scope size n.
     function case(ϕ::Diagram, n::Integer; atol::Real = 0)
         # All possible valuations (including impossible ones).
-        M = all_valuations(collect(1:n))
+        M = BDD.all_valuations(collect(1:n))
         # Get only possible worlds.
         W = M[findall(ϕ.(eachrow(M))),:]
         # Assign random probabilities for each world in W.
@@ -24,10 +25,10 @@ using BinaryDecisionDiagrams
         @test isapprox(evi[findall(>(0), evi)], (R/sum(R)); atol)
     end
 
-    case((1 ∧ 2) ∨ (3 ∧ ¬4) ∨ (¬1 ∧ 5), 5)
-    case((1 → 3) ∧ (5 → ¬2), 5)
-    case(and(1, 2, 3) ∨ and(4, 5), 5)
-    case(exactly(3, collect(1:5)), 5)
-    case(atleast(3, collect(1:5)), 5)
-    case(atmost(3, collect(1:5)), 5)
+    case(BDD.or(BDD.and(1, 2), BDD.and(3, BDD.:¬(4)), BDD.and(BDD.:¬(1), 5)), 5)
+    case(BDD.and(BDD.:→(1, 3), BDD.:→(5, BDD.:¬(2))), 5)
+    case(BDD.or(BDD.and(1, 2, 3), BDD.and(4, 5)), 5)
+    case(BDD.exactly(3, collect(1:5)), 5)
+    case(BDD.atleast(3, collect(1:5)), 5)
+    case(BDD.atmost(3, collect(1:5)), 5)
 end
