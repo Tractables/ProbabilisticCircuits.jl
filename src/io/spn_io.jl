79c23b2e6733c583476882948165817839d79032
Guy Van den Broeck <guyvdb@gmail.com>
2021-10-05 20:33:18 -0700

    `.spn` format parser

---
+++ src/io/spn_io.jl
@@ -0,0 +1,130 @@
+export zoo_spn, zoo_spn_file, 
+    SpnFormat, SpnVtreeFormat
+
+struct SpnFormat <: FileFormat end
+
+##############################################
+# Read SPN format (some dialect of the Libra AC format?)
+##############################################
+
+zoo_spn_file(name) = 
+    artifact"circuit_model_zoo" * zoo_version * "/spns/$name"
+
+"""
+    zoo_spn(name)
+
+Loads SPN file with given name from model zoo. See https://github.com/UCLA-StarAI/Circuit-Model-Zoo.    
+"""
+zoo_spn(name) = 
+    read(zoo_spn_file(name), ProbCircuit, SpnFormat())
+
+const spn_grammar = raw"""
+    start : domains (_NL node)+ _NL "EOF" _NL?
+
+    domains : "(2" (_WS "2")* ")"
+    
+    node : "v" _WS INT _WS INT -> literal_node
+         | "*" (_WS INT)+ -> prod_node
+         | "+" (_WS INT _WS LOGPROB)+ -> sum_node
+         
+    %import common.INT
+    %import common.SIGNED_NUMBER -> LOGPROB
+    %import common.WS_INLINE -> _WS
+    %import common.NEWLINE -> _NL
+    """
+
+const spn_parser = Lark(spn_grammar)
+
+struct SpnParse <: JuiceTransformer
+    nodes::Vector{PlainProbCircuit}
+    SpnParse() = new(PlainProbCircuit[])
+end
+
+@rule start(t::SpnParse, x) =
+    t.nodes[end]
+
+@inline domains(t::SpnParse, x) = begin 
+    d = Base.parse.(Int,x)
+    @assert d .== 2 "Only Boolean domains are currently supported, not $d in SPN format"
+    d
+end
+
+@rule literal_node(t::SpnParse, x) = begin
+    v = Base.parse(Var,x[1]) + 1
+    @assert x[2] == "0" || x[2] == "1" "Boolean domains only." 
+    l = (x[2] == 1) ? Lit(v) : -Lit(v)
+    push!(t.nodes, PlainProbLiteralNode(l))
+end
+
+@rule prod_node(t::SpnParse,x) = begin
+    child_i = Base.parse.(Int,x) .+ 1
+    children = t.nodes[child_i]
+    push!(t.nodes, PlainMulNode(children))
+end
+
+@rule sum_node(t::SpnParse,x) = begin
+    child_i = Base.parse.(Int,x[1:2:end]) .+ 1
+    children = t.nodes[child_i]
+    log_probs = Base.parse.(Float64,x[2:2:end])
+    push!(t.nodes, PlainSumNode(children, log_probs))
+end
+
+function Base.parse(::Type{PlainProbCircuit}, str, ::SpnFormat) 
+    ast = Lerche.parse(spn_parser, str)
+    Lerche.transform(SpnParse(), ast)
+end
+
+Base.read(io::IO, ::Type{PlainProbCircuit}, ::SpnFormat) =
+    parse(PlainProbCircuit, read(io, String), SpnFormat())
+
+
+# ##############################################
+# # Write SPNs
+# ##############################################
+
+# const SPN_FORMAT = """c this file was saved by ProbabilisticCircuits.jl
+# c ids of spn nodes start at 0
+# c spn nodes appear bottom-up, children before parents
+# c
+# c file syntax:
+# c spn count-of-spn-nodes
+# c L id-of-literal-spn-node id-of-vtree literal
+# c P id-of-sum-spn-node id-of-vtree number-of-children {child-id}+
+# c S id-of-product-spn-node id-of-vtree number-of-children {child-id}+ {log-probability}+
+# c"""
+
+# function Base.write(io::IO, circuit::ProbCircuit, ::SpnFormat, vtreeid::Function = (x -> 0))
+
+#     labeling = label_nodes(circuit)
+#     map!(x -> x-1, values(labeling)) # vtree nodes are 0-based indexed
+
+#     println(io, SPN_FORMAT)
+#     println(io, "spn $(num_nodes(circuit))")
+#     foreach(circuit) do n
+#         if isliteralgate(n)
+#             println(io, "L $(labeling[n]) $(vtreeid(n)) $(literal(n))")
+#         elseif isconstantgate(n)
+#             @assert false
+#         else
+#             t = is⋀gate(n) ? "P" : "S"
+#             print(io, "$t $(labeling[n]) $(vtreeid(n)) $(num_children(n))")
+#             if is⋀gate(n)
+#                 for child in children(n)
+#                     print(io, " $(labeling[child])")
+#                 end
+#             else
+#                 @assert is⋁gate(n)  
+#                 for (child, logp) in zip(children(n), n.log_probs)
+#                     print(io, " $(labeling[child]) $logp")
+#                 end    
+#             end
+#             println(io)
+#         end
+#     end
+#     nothing
+# end
+
+# function Base.write(ios::Tuple{IO,IO}, circuit::StructProbCircuit, format::SpnVtreeFormat)
+#     vtree2id = write(ios[2], vtree(circuit), format[2])
+#     write(ios[1], circuit, format[1], n -> vtree2id[vtree(n)])
+# end
