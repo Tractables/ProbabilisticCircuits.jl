fdf1b71af0e451d90fad6d22f4b7db8b0fb32506
Guy Van den Broeck <guyvdb@cs.ucla.edu>
2022-02-02 22:56:58 -0800

    richer experiments

--- example/single_experiment.jl
+++ example/single_experiment.jl
@@ -7,8 +7,8 @@ println("Loading Data")
 include("load_mnist.jl");
 train, test = mnist_gpu()
 
-function experiment(train, test, epochs1, epochs2, latents;
-    batch_size=512, pseudocount, softness, param_inertia=0.0, param_inertia_end=1.0, shuffle=:each_batch)
+function experiment(train, test, epochs1, epochs2, epochs3, latents;
+    batch_size=512, pseudocount, softness, param_inertia1, param_inertia_end1, param_inertia2, param_inertia_end2, shuffle=:each_epoch)
 
     println("Generating HCLT structure with $latents latents")
     circuit = hclt(train; num_cats=2, num_hidden_cats = latents)
@@ -20,17 +20,26 @@ function experiment(train, test, epochs1, epochs2, latents;
     bpc = CuBitsProbCircuit(BitsProbCircuit(circuit))
 
     println("mini_batch_em(bpc, train, $epochs1; 
-        batch_size=$batch_size, pseudocount=$pseudocount, softness=$softness, param_inertia=$param_inertia, param_inertia_end=$param_inertia_end, shuffle=$shuffle)")
+        batch_size=$batch_size, pseudocount=$pseudocount, softness=$softness, param_inertia=$param_inertia1, param_inertia_end=$param_inertia_end1, shuffle=$shuffle)")
     
     @time mini_batch_em(bpc, train, epochs1; 
-        batch_size, pseudocount, softness, param_inertia, param_inertia_end, shuffle)
+        batch_size, pseudocount, softness, param_inertia = param_inertia1, param_inertia_end = param_inertia_end1, shuffle)
 
     print("Test set log likelihood is ")
     println(loglikelihood(test, bpc; batch_size))
 
-    println("full_batch_em(bpc, train, $epochs2; 
+    println("mini_batch_em(bpc, train, $epochs2; 
+        batch_size=$batch_size, pseudocount=$pseudocount, softness=$softness, param_inertia=$param_inertia2, param_inertia_end=$param_inertia_end2, shuffle=$shuffle)")
+    
+    @time mini_batch_em(bpc, train, epochs2; 
+        batch_size, pseudocount, softness, param_inertia=param_inertia2, param_inertia_end=param_inertia_end2, shuffle)
+
+    print("Test set log likelihood is ")
+    println(loglikelihood(test, bpc; batch_size))
+
+    println("full_batch_em(bpc, train, $epochs3; 
     batch_size=$batch_size, pseudocount=$pseudocount, softness=$softness)")
-    @time full_batch_em(bpc, train, epochs2; 
+    @time full_batch_em(bpc, train, epochs3; 
         batch_size, pseudocount, softness)
 
     print("Test set log likelihood is ")
