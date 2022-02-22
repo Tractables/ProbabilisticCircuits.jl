afae957c04611a410180a4fa62fe98645776016d
Guy Van den Broeck <guyvdb@cs.ucla.edu>
2022-02-21 21:59:49 -0800

    omit plot rendering tests

--- test/io/plot_tests.jl
+++ test/io/plot_tests.jl
@@ -1,5 +1,5 @@
 using Test, ProbabilisticCircuits
-using TikzPictures
+# using TikzPictures
 
 include("../helper/plain_dummy_circuits.jl")
 
@@ -7,18 +7,19 @@ include("../helper/plain_dummy_circuits.jl")
     
     mktempdir() do tmp
             
+        # Note: omitting rendering tests to speed up CI
 
         pc = little_3var()
         p = @test_nowarn plot(pc)
-        @test_nowarn save(SVG("$tmp/example1.svg"), p)
+        # @test_nowarn save(SVG("$tmp/example1.svg"), p)
 
         pc = little_3var_categorical()
         p = @test_nowarn plot(pc)
-        @test_nowarn save(SVG("$tmp/example2.svg"), p)
+        # @test_nowarn save(SVG("$tmp/example2.svg"), p)
 
         pc = little_hybrid_circuit()
         p = @test_nowarn plot(pc)
-        @test_nowarn save(SVG("$tmp/example3.svg"), p)
+        # @test_nowarn save(SVG("$tmp/example3.svg"), p)
     
     end
 end
