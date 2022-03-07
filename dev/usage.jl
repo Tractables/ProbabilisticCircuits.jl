eb44312e7805dc028310e80841ca82b0fe73ff92
Documenter.jl <documenter@juliadocs.github.io>
2022-03-07 23:41:36 +0000

    build based on 5437fae

--- dev/usage.jl
+++ dev/usage.jl
@@ -7,29 +7,30 @@ using ProbabilisticCircuits
 # ### Reasoning with manually constructed circuits
 
 # We begin by creating three positive literals (boolean variables) and manually construct a probabilistic circuit that encodes a Naive Bayes (NB) distribution with the following form: `Pr(rain, rainbow, wet) = Pr(rain) * Pr(rainbow|rain) * Pr(wet|rain)`.
-rain, rainbow, wet = pos_literals(ProbCircuit, 3)
-rain_pos = (0.7 * rainbow + 0.3 * (-rainbow)) * (0.9 * wet + 0.1 * (-wet)) # Pr(rainbow|rain=1) * Pr(wet|rain=1)
-rain_neg = (0.2 * rainbow + 0.8 * (-rainbow)) * (0.3 * wet + 0.7 * (-wet)) # Pr(rainbow|rain=0) * Pr(wet|rain=0)
-circuit = 0.4 * (rain * rain_pos) + 0.6 * ((-rain) * rain_neg); # Pr(rain, rainbow, wet)
+
+rain, rainbow, wet = [ProbabilisticCircuits.PlainInputNode(i, Indicator(true)) for i=1:3]
+rain_, rainbow_, wet_ = [ProbabilisticCircuits.PlainInputNode(i, Indicator(false)) for i=1:3]
+
+rain_pos = (0.7 * rainbow + 0.3 * (rainbow_)) * (0.9 * wet + 0.1 * (wet_)) # Pr(rainbow|rain=1) * Pr(wet|rain=1)
+rain_neg = (0.2 * rainbow + 0.8 * (rainbow_)) * (0.3 * wet + 0.7 * (wet_)) # Pr(rainbow|rain=0) * Pr(wet|rain=0)
+circuit = 0.4 * (rain * rain_pos) + 0.6 * ((rain_) * rain_neg); # Pr(rain, rainbow, wet)
 
 # Just like any probability distribution, we can evaluate the probabilistic circuit on various inputs. Note that since log probabilities are used in probabilistic circuits for numerical stability, we need to take exponent of the evaluation output to get the probabilities.
-exp(circuit(true, true, true)) # Pr(rain=1, rainbow=1, wet=1)
+exp(loglikelihoods(circuit, [[true, true, true]]; batch_size = 1)) # Pr(rain=1, rainbow=1, wet=1)
 #-
-exp(circuit(true, false, false)) # Pr(rain=1, rainbow=0, wet=0)
+exp(loglikelihoods(circuit, [[true, false, false]]; batch_size = 1)) # Pr(rain=1, rainbow=0, wet=0)
 
 # From the above examples, we see that it is less likely to rain if we do not see rainbows and the streets are not wet.
 
-# The purpose of this package is to offer a unified tool for efficient learning and inference (i.e., answering probabilistic queries such as marginals and MAP) over probabilistic circuits, which subsume a large class of tractable probabilistic models. We first use the above manually constructed circuit to demonstrate several queries that can be answered efficiently. Similar to [logic circuits](https://github.com/Juice-jl/LogicCircuits.jl), answering the following queries require *decomposability* and *determinism*, which is already satisfied by construction:
-isdecomposable(circuit) && isdeterministic(circuit)
-
 # Decomposability allows us to compute marginal probabilities given partial evidence efficiently (linear time w.r.t. the circuit size). For example, we want to ask the probability of observing rainbows. That is, we want to marginalize out the variables rain and wet. This can be done by evaluating the circuit with partial evidence:
-exp(circuit(missing, true, missing)) # Pr(rainbow=1)
+
+exp(loglikelihoods(circuit, [[missing, true, missing]]; batch_size = 1))
 
 # Being able to compute marginals immediately offers the ability to compute conditional probabilities. For example, to compute the probability of raining given rainbow=1 and wet=1, we simply take the quotient of Pr(rain=1, rainbow=1, wet=1) and Pr(rainbow=1, wet=1):
 exp(circuit(true, true, true) - circuit(missing, true, true)) # Pr(rain=1|rainbow=1, wet=1)
 
 # If we are additionally supplied with the structural property *determinism*, we can answer some more advanced queries. For example, we can to compute the maximum a posteriori (MAP) query of the distribution:
-assignments, log_prob = MAP(circuit, [missing, missing, missing])
+assignments, log_prob = MAP(circuit, [missing, missing, missing]; batch_size=1)
 print("The MAP assignment of the circuit is (rain=$(assignments[1]), rainbow=$(assignments[2]), wet=$(assignments[3])), with probability $(exp(log_prob)).")
 
 # Besides the above examples, ProbabilisticCircuits.jl provides functionalities for a wide variety of queries, which are detailed in [this manual](https://juice-jl.github.io/ProbabilisticCircuits.jl/stable/manual/queries/).
