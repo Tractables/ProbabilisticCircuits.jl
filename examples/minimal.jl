b71ad40e60ad35ced24bf0ce69d4c605803ca311
Pasha Khosravi <khosravipasha@gmail.com>
2022-03-07 18:46:48 -0800

    update minimal

--- examples/minimal.jl
+++ examples/minimal.jl
@@ -6,6 +6,11 @@ struct Indicator{T} <: InputDist
     value::T
 end
 const Literal = Indicator{Bool}
+
+struct Categorical <: InputDist
+    categories::UInt32
+end
+
 ######
 abstract type BitsNode end
 struct BitsSum <: BitsNode end
@@ -23,25 +28,33 @@ function kernel(ans, vec, ids)
         item = vec[cur_id]
         d = dist(item)::InputDist
         var = item.variable
-        if d isa Literal
-            CUDA.@atomic ans[var] += 1
-        end
+        
+        CUDA.@atomic ans[ans_idx(d)] += 1
     end
     return nothing
 end
 
+ans_idx(n::Indicator{Bool}) = 1
+ans_idx(n::Indicator{UInt32}) = 2
+ans_idx(n::Categorical) = 3
+
 ########
 
 function run()
-    vec = Vector{Union{BitsSum, BitsInput{Indicator{Bool}}, BitsInput{Indicator{UInt32}}}}()
+    vec = Vector{Union{BitsSum, 
+                       BitsInput{Indicator{Bool}}, 
+                       BitsInput{Indicator{UInt32}},
+                       BitsInput{Categorical}
+                       }}()
     input_node_ids = [1, 2]
     push!(vec, BitsInput(1, Indicator{Bool}(false)))
     push!(vec, BitsInput(2, Indicator{UInt32}(1)))
+    push!(vec, BitsInput(2, Categorical(10)))
     push!(vec, BitsSum())
 
     cuvec = cu(vec)
     ids = cu(input_node_ids)
-    ans = CUDA.zeros(Int64, 2)
+    ans = CUDA.zeros(Int64, 3)
 
     @device_code_warntype @cuda kernel(ans, cuvec, ids)
     println(ans)
